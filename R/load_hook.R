158dc303b3cead98a42e9d138b7e1795e949631d
Dewey Dunnington <dewey@fishandwhistle.net>
2017-09-27 18:54:49 -0400

    fix attach message

--- R/load_hook.R
+++ R/load_hook.R
@@ -2,5 +2,9 @@
 # .onLoad hook for when namespace is loaded
 .onLoad <- function(libname, pkgname) {
   configure_exiftool(quiet = TRUE, install_url = TRUE)
-  message("Using ExifTool version ", exiftool_version())
+}
+
+# .onAttach for library(exifr)
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Using ExifTool version ", exiftool_version())
 }
